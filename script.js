2130683750064c353182a50ecf76a482892d2778
Gergely Izsak <izsak.gera@gmail.com>
2021-06-29 14:33:58 +0200

    basic 1 finished

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function createElement() {
-    let a = document.getElementsByName("firstname")[0].value;
-    let b = document.getElementsByName("lastname")[0].value;
-    let c = document.getElementsByName("age")[0].value;
-    document.getElementById("result").innerHTML += a + " " + b + " " + c + "<br>";
-    document.getElementsByClassName("input")[0].value = "";
+  let a = document.getElementsByName('firstname')[0].value;
+  let b = document.getElementsByName('lastname')[0].value;
+  let c = document.getElementsByName('age')[0].value;
+  document.getElementById('result').innerHTML += a + ' ' + b + ' ' + c + '<br>';
+  //   document.getElementsByClassName("input")[0].value = '';
+  event.preventDefault();
 }
-document.getElementById("btn").addEventListener("click", function () {
-    createElement();
-});
+
+document.getElementById('btn').addEventListener('click', createElement, false);
